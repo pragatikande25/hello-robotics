63021eed838cb9f9d6dd493c9b6e3673ce873683
pragatikande25 <pragatikande25@gmail.com>
2025-09-29 16:35:23 +0530

    Create Program 61.c

---
+++ Program 61.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    if(n % 7 == 0) printf("Divisible by 7");
+    else printf("Not Divisible by 7");
+    return 0;
+}
