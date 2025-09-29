1fce400f18d6402b061d5d713ab23c8f61efd9b3
pragatikande25 <pragatikande25@gmail.com>
2025-09-29 16:57:40 +0530

    Create Program 80.c

---
+++ Program 80.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int d;
+    printf("Enter day number (1-7): ");
+    scanf("%d", &d);
+    if(d==1) printf("Sunday");
+    else if(d==2) printf("Monday");
+    else if(d==3) printf("Tuesday");
+    else if(d==4) printf("Wednesday");
+    else if(d==5) printf("Thursday");
+    else if(d==6) printf("Friday");
+    else if(d==7) printf("Saturday");
+    else printf("Invalid Day");
+    return 0;
+}
