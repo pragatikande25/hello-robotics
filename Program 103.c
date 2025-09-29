fd828a54a6e86cb5cd884e10a4bbd0829b9ca6de
Pragati kande <pragatikande25@gmail.com>
2025-09-29 18:13:45 +0530

    Create Program 103.c

---
+++ Program 103.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    if(a<=b && a<=c) printf("%d is smallest", a);
+    else if(b<=c) printf("%d is smallest", b);
+    else printf("%d is smallest", c);
+    return 0;
+}
