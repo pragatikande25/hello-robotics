063f946d736d83e6c781404240e4f91583c47e8e
pragatikande25 <pragatikande25@gmail.com>
2025-09-29 16:54:46 +0530

    Create Program 77.c

---
+++ Program 77.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    if(n > 0) printf("Positive");
+    else if(n < 0) printf("Negative");
+    else printf("Zero");
+    return 0;
+}
