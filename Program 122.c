952bbccbf64dc75bbff7d232a0747dbe6f2f0b76
Pragati kande <pragatikande25@gmail.com>
2025-09-29 18:36:07 +0530

    Create Program 122.c

---
+++ Program 122.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+    int n, max;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    max=arr[0];
+    for(int i=1;i<n;i++) if(arr[i]>max) max=arr[i];
+    printf("Max=%d",max);
+    return 0;
+}
