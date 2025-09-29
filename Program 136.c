91c5c8e094b2af4e04ea2adfa1c7972cdca17c02
Pragati kande <pragatikande25@gmail.com>
2025-09-29 18:50:13 +0530

    Create Program 136.c

---
+++ Program 136.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() 
+{
+    char str[100];
+    scanf("%s",str);
+    for(int i=0;str[i];i++) str[i]=tolower(str[i]);
+    printf("%s",str);
+    return 0;
+}
