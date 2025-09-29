2cc5b167b7b12709ba0e50a65ff79c6812ca6725
pragatikande25 <pragatikande25@gmail.com>
2025-09-29 16:37:26 +0530

    Create Program 64.c

---
+++ Program 64.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    if(ch >= 'A' && ch <= 'Z') printf("Uppercase");
+    else if(ch >= 'a' && ch <= 'z') printf("Lowercase");
+    else printf("Not an alphabet");
+    return 0;
+}
